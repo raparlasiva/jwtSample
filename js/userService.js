8a4dba57b8de1ad71423a26b7e6d079a8d51f0bf
sraparla <sraparla@indyimaging.com>
2014-10-24 17:17:34 -0400

    userService added

---
+++ js/userService.js
@@ -0,0 +1,34 @@
+(function() {
+'use strict';
+    var userFactoryServices = angular.module('userFactoryServices', [])
+    .constant('API_URL','http://'+location.hostname+'/angular-codeigniter-seed-maste/api/account');
+    
+    userFactoryServices.factory('userFactory',['authTokenFactory','$http','API_URL', function userFactory(authTokenFactory,$http,API_URL) {
+        alert("here");
+        return {
+            login: login,
+            logout: logout
+        };
+        
+        function login(email, password) {
+            alert("inside userfatory login function");
+            return $http.post('api/account/login', {
+              email: email,
+              password: password
+            }).then(function success(response) {
+                console.info(response);
+                alert("inside userfatory login success");
+                authTokenFactory.setToken(response.data.token);
+                return response;
+            });
+        };
+        
+        function logout() {
+            authTokenFactory.setToken();
+        }
+        
+    }]);
+    
+}());
+
+
